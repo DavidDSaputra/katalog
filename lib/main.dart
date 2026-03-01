e04735e791e066cf3ff3f8883f95b7865f2aa006
David D Saputra <david@example.com>
2026-03-01 19:33:21 +0700

    HALAMAN KERANJANG: MyCart dengan watch provider & main integrate

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const MyCatalog(),
-        '/cart': (context) => const MyCartPlaceholder(),
+        '/cart': (context) => const MyCart(),
       },
     );
   }
@@ -103,8 +103,38 @@ class AddButton extends StatelessWidget {
   }
 }
 
-class MyCartPlaceholder extends StatelessWidget {
-  const MyCartPlaceholder({super.key});
+class MyCart extends StatelessWidget {
+  const MyCart({super.key});
+
   @override
-  Widget build(BuildContext context) => const Scaffold(body: Text('Cart'));
+  Widget build(BuildContext context) {
+    var cart = context.watch<CartModel>();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Keranjang Belanja')),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (context, index) => ListTile(
+                leading: const Icon(Icons.fastfood),
+                title: Text(cart.items[index]),
+              ),
+            ),
+          ),
+          const Divider(),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: ElevatedButton(
+              onPressed: () => cart.removeall(),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              child: const Text('Hapus Keranjang',
+                  style: TextStyle(color: Colors.white)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
