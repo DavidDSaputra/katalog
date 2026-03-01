bfec654e4bb6e00371f377949f17ef32501faccb
David D Saputra <david@example.com>
2026-03-01 19:31:35 +0700

    state model: CartModel dengan ChangeNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,38 @@ import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text('Setup Provider')),
-      ),
+    ChangeNotifierProvider<CartModel>(
+      create: (context) => CartModel(),
+      child: const MyApp(),
     ),
   );
+}
+
+// 1. state model (business logic)
+class CartModel extends ChangeNotifier {
+  final List<String> _items = [];
+
+  List<String> get items => _items;
+
+  void add(String item) {
+    _items.add(item);
+    notifyListeners();
+  }
+
+  void removeall() {
+    _items.clear();
+    notifyListeners();
+  }
+}
+
+// 2. UI layer
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(body: Text('State Model Added')),
+    );
+  }
 }
